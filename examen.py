b589843e4eb58a7aa1a6ccce3b7d88a9756846b2
Gerardo02 <gerardo.jerez@hotmail.com>
2020-09-10 07:44:47 -0700

    segunda

--- examen.py
+++ examen.py
@@ -4,17 +4,19 @@ sys.path.insert(1,'dsp-modulo')
 from thinkdsp import SinSignal
 from thinkdsp import decorate
 
+#697Hz + 1477Hz
+frecuencia3 = SinSignal(freq=2174, amp=1, offset=0)
 
-frecuencia3 = SinSignal(freq=697, amp=1, offset=0)
-frecuencia7 = SinSignal(freq=852, amp=1, offset=0)
-frecuencia32 = SinSignal(freq=697, amp=1, offset=0)
-frecuencia33 = SinSignal(freq=697, amp=1, offset=0)
+#852Hz + 1209Hz
+frecuencia7 = SinSignal(freq=2061, amp=1, offset=0)
 
-wave_697 = frecuencia3.make_wave(duration=0.5, start=0, framerate=44100)
-wave_852 = frecuencia7.make_wave(duration=0.5, start=0.5, framerate=44100)
-wave_697_2 = frecuencia32.make_wave(duration=0.5, start=1.0, framerate=44100)
-wave_697_3 = frecuencia33.make_wave(duration=0.5, start=1.5, framerate=44100)
 
-wave_sonido = wave_697 + wave_852 + wave_697_2 + wave_697_3
+
+wave_2174 = frecuencia3.make_wave(duration=0.5, start=0, framerate=44100)
+wave_2061 = frecuencia7.make_wave(duration=0.5, start=0.5, framerate=44100)
+wave_2174_2 = frecuencia3.make_wave(duration=0.5, start=1.0, framerate=44100)
+wave_2174_3 = frecuencia3.make_wave(duration=0.5, start=1.5, framerate=44100)
+
+wave_sonido = wave_2174 + wave_2061 + wave_2174_2 + wave_2174_3
 
 wave_sonido.write("output.wav")
