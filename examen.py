3401d20979fa384ca9d7218e0fada3b6bca351b9
Gerardo02 <gerardo.jerez@hotmail.com>
2020-09-10 08:12:06 -0700

    hola

--- examen.py
+++ examen.py
@@ -5,18 +5,31 @@ from thinkdsp import SinSignal
 from thinkdsp import decorate
 
 #697Hz + 1477Hz
-frecuencia3 = SinSignal(freq=2174, amp=1, offset=0)
-
+frecuencia3 = SinSignal(freq=697, amp=1, offset=0)
+frecuencia32 = SinSignal(freq=1477, amp=1, offset=0)
 #852Hz + 1209Hz
-frecuencia7 = SinSignal(freq=2061, amp=1, offset=0)
+frecuencia7 = SinSignal(freq=852, amp=1, offset=0)
+frecuencia72 = SinSignal(freq=1209, amp=1, offset=0)
 
+wave_prueba = frecuencia3.make_wave(duration=0.5, start=0, framerate=44100)
+wave_prueba2 = frecuencia32.make_wave(duration=0.5, start=0, framerate=44100)
 
+sonido3 = wave_prueba + wave_prueba2
 
-wave_2174 = frecuencia3.make_wave(duration=0.5, start=0, framerate=44100)
-wave_2061 = frecuencia7.make_wave(duration=0.5, start=0.5, framerate=44100)
-wave_2174_2 = frecuencia3.make_wave(duration=0.5, start=1.0, framerate=44100)
-wave_2174_3 = frecuencia3.make_wave(duration=0.5, start=1.5, framerate=44100)
+wave_prueba3 = frecuencia7.make_wave(duration=0.5, start=0.5, framerate=44100)
+wave_prueba4 = frecuencia72.make_wave(duration=0.5, start=0.5, framerate=44100)
 
-wave_sonido = wave_2174 + wave_2061 + wave_2174_2 + wave_2174_3
+wave_prueba5 = frecuencia3.make_wave(duration=0.5, start=1.0, framerate=44100)
+wave_prueba6 = frecuencia32.make_wave(duration=0.5, start=1.0, framerate=44100)
 
-wave_sonido.write("output.wav")
+wave_prueba7 = frecuencia3.make_wave(duration=0.5, start=1.5, framerate=44100)
+wave_prueba8 = frecuencia32.make_wave(duration=0.5, start=1.5, framerate=44100)
+
+sonido7 = wave_prueba3 + wave_prueba4
+
+sonido32 = wave_prueba5 + wave_prueba6
+sonido33 = wave_prueba7 + wave_prueba8
+
+sonido4 = sonido3 + sonido7 + sonido32 + sonido33
+
+sonido4.write("output.wav")
